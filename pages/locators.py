037430fd4099b4c66eec80a6a78ab20f973f4de2
Ali Sadretdinov <sadrali1990@gmail.com>
2022-10-23 15:17:13 +0300

    tests for product page. checked add to basket button, prices and names of product and prices and names in basket

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,12 @@ class LoginPageLocators():
     LOGIN_FORM = (By.ID, "login_form")
     REG_FORM = (By.ID, "register_form")
 
+class ProductPageLocators():
+    PRODUCT_PRICE = (By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/p[1]')
+    PRODUCT_NAME = (By.XPATH, '/html/body/div[2]/div/div[2]/div[2]/article/div[1]/div[2]/h1')
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
+    BASKET_TOTAL_AMOUNT = (By.XPATH, '/html/body/div[2]/div/div[1]/div[3]/div/p[1]/strong')
+    ADDED_PRODUCT_NAME = (By.XPATH, '/html/body/div[2]/div/div[1]/div[1]/div/strong')
+
+
+
---
+++ pages/product_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON).click()
+
+    def product_has_been_added_to_your_basket(self):
+        actual_name = self.browser.find_element(*ProductPageLocators.ADDED_PRODUCT_NAME).text
+        expected_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        assert expected_name == actual_name, f"actual name is {actual_name} but expected name is {expected_name}"
+
+    def basket_price_is_equal_product_price(self):
+        actual_price = self.browser.find_element(*ProductPageLocators.BASKET_TOTAL_AMOUNT).text
+        expected_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        assert actual_price == expected_price, f"actual price is {actual_price} but expected price is {expected_price}"
