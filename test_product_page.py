4dc484e526d0604979c4c20527e9675448ded11f
Ali Sadretdinov <sadrali1990@gmail.com>
2022-10-26 01:41:23 +0300

    commit for transfer current changes to another pc

--- test_product_page.py
+++ test_product_page.py
@@ -9,5 +9,3 @@ def test_guest_can_add_product_to_basket(browser):
     page.solve_quiz_and_get_code()
     page.product_has_been_added_to_your_basket()
     page.basket_price_is_equal_product_price()
-
-
