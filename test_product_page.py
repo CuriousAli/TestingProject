a92a51a9f97617260c7c6877cc75fbebf3f1c536
Ali <elvira@Elviras-MacBook-Pro.local>
2022-11-04 01:23:27 +0300

    Added back promo test's parametrizes for test_guest_can_add_product_to_basket

--- test_product_page.py
+++ test_product_page.py
@@ -6,8 +6,12 @@ import time
 
 
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
-    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+@pytest.mark.parametrize('link', ["?promo=offer0", "?promo=offer1", "?promo=offer2",
+                                  "?promo=offer3", "?promo=offer4", "?promo=offer5",
+                                  "?promo=offer6", pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                  "?promo=offer8", "?promo=offer9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link}"
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
